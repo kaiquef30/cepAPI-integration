42f297fea76b8de31b2f698827b35a0d5ee90750
kaiquef30 <kaiquefernandosantana@gmail.com>
2023-12-02 13:02:40 -0300

    refactor

--- src/main/java/com/apicepintegration/apiintegration/resources/CEPClientResource.java
+++ src/main/java/com/apicepintegration/apiintegration/resources/CEPClientResource.java
@@ -1,28 +1,30 @@
-package com.apicepintegration.apiintegration.controller;
+package com.apicepintegration.apiintegration.resources;
+
 
 import com.apicepintegration.apiintegration.model.CEPClientModel;
 import com.apicepintegration.apiintegration.service.CEPClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/user")
-public class ControllerUser {
+@RequestMapping("/search")
+public class CEPClientResource {
 
     private final CEPClientService cepClientService;
 
-
-    public ControllerUser(CEPClientService cepClientService) {
+    public CEPClientResource(CEPClientService cepClientService) {
         this.cepClientService = cepClientService;
     }
 
-    @GetMapping("/allZip-code")
-    public ResponseEntity<List<CEPClientModel>> getAllZipCodeRegistered(){
-        return ResponseEntity.status(HttpStatus.OK).body(cepClientService.searchRegisteredZipCode());
+
+    @GetMapping("/{cep}")
+    public ResponseEntity<CEPClientModel> getCEPSpecified(@PathVariable String cep) {
+        return ResponseEntity.status(HttpStatus
+                .OK).body(cepClientService.getCEPSpecified(cep));
     }
+
 }
--- src/main/java/com/apicepintegration/apiintegration/resources/UserResource.java
+++ src/main/java/com/apicepintegration/apiintegration/resources/UserResource.java
@@ -1,30 +1,29 @@
-package com.apicepintegration.apiintegration.controller;
-
+package com.apicepintegration.apiintegration.resources;
 
 import com.apicepintegration.apiintegration.model.CEPClientModel;
 import com.apicepintegration.apiintegration.service.CEPClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/search")
-public class CEPClientController {
+@RequestMapping("/user")
+public class UserResource {
 
     private final CEPClientService cepClientService;
 
-    public CEPClientController(CEPClientService cepClientService) {
+
+    public UserResource(CEPClientService cepClientService) {
         this.cepClientService = cepClientService;
     }
 
-
-    @GetMapping("/{cep}")
-    public ResponseEntity<CEPClientModel> getCEPSpecified(@PathVariable String cep) {
-        return ResponseEntity.status(HttpStatus
-                .OK).body(cepClientService.getCEPSpecified(cep));
+    @GetMapping("/allZip-code")
+    public ResponseEntity<List<CEPClientModel>> getAllZipCodeRegistered(){
+        return ResponseEntity.status(HttpStatus.OK).body(cepClientService.searchRegisteredZipCode());
     }
 
 }
